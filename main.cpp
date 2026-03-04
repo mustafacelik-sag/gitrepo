782686aeaea356e778a690eb2ffe42677415ad7f
mustafacelik-sag <mustafa.celik2528@hotmail.com>
2026-03-04 12:09:51 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 using namespace std;
-
+string name = "Mustafa";
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Hello git! Enter name:" << endl;
+    cin >> name;
+    cout << "Your name is: " << name << endl;
     return 0;
 }
